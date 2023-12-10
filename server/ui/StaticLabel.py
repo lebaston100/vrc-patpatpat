45d3c24276e48fdaccd82776f7aff4495f1cf910
lebaston100 <26111114+lebaston100@users.noreply.github.com>
2023-12-10 14:08:06 +0100

    add a static label class

---
+++ server/ui/StaticLabel.py
@@ -0,0 +1,38 @@
+"""A simple QLabel expansion with a static text prefix.
+
+Typical usage example:
+
+    myLabel = StaticLabel("Name: ") -> "Name: "
+    myLabel = StaticLabel("Name: ", "Initial Text") -> "Name: Initial Text"
+    myLabel.setText("patpatpat") -> "Name: patpatpat"
+"""
+
+from PyQt6.QtWidgets import QLabel
+
+from utils import LoggerClass
+logger = LoggerClass.getSubLogger(__name__)
+
+
+class StaticLabel(QLabel):
+    """A simple expansion of QLabel with a static prefix text.
+    """
+
+    def __init__(self, prefixText: str, text: [str | None] = None, *args, **kwargs) -> None:
+        """Initialize the label.
+
+        Args:
+            prefixText (str): The static prefix text
+        """
+
+        super().__init__(*args, **kwargs)
+        self._prefixText = prefixText
+        self.setText(text or "")
+
+    def setText(self, text: str) -> None:
+        """Update label text.
+
+        Args:
+            text (str): The new text to set the label to
+        """
+
+        return super().setText(self._prefixText + text)
--- server/ui/__init__.py
+++ server/ui/__init__.py
@@ -1,3 +1,3 @@
-from .TimerWorker import TimerWorker
 from .LogWindow import LogWindow
 from .ToggleButton import ToggleButton
+from .StaticLabel import StaticLabel
