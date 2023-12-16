2e5e0bd18156f3f8fdc4898a9a0117de229f410b
lebaston100 <26111114+lebaston100@users.noreply.github.com>
2023-12-16 19:55:38 +0100

    Add ui helper class for exit events

---
+++ server/ui/uiHelpers.py
@@ -0,0 +1,38 @@
+"""Some helper functions and classes for use in the UI.
+
+Exposes:
+    handleCloseEvent: A simple message box popup to reject a close event
+        when a dialog has unsaved changes
+
+"""
+
+from PyQt6.QtGui import QCloseEvent
+from PyQt6.QtWidgets import QMessageBox, QWidget
+
+
+def handleCloseEvent(parent: QWidget | None, event: QCloseEvent) -> bool:
+    """Asks the use if they want to close the settings with without saving.
+
+    Args:
+        parent (QWidget | None): The parent QWidget.
+        event (QCloseEvent): The QCloseEvent to hande.
+
+    Returns:
+        bool: If event was accepted or not.
+    """
+    result = QMessageBox.question(parent, "Unsaved changes",
+                                  "You have unsaved changes, do you "
+                                  + "want to close anyways?",
+                                  QMessageBox.StandardButton.Cancel |
+                                  QMessageBox.StandardButton.Yes,
+                                  QMessageBox.StandardButton.Cancel)
+    if result == QMessageBox.StandardButton.Yes:
+        event.accept()
+        return True
+    else:
+        event.ignore()
+        return False
+
+
+if __name__ == "__main__":
+    print("There is no point running this file directly")
