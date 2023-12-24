79f17d436e16b32a98ca115f45cfa9d369d53c13
lebaston100 <26111114+lebaston100@users.noreply.github.com>
2023-12-24 11:16:28 +0100

    create custom float delegate

---
+++ server/ui/Delegates.py
@@ -0,0 +1,68 @@
+"""
+This module provides a custom item delegate for handling float values in
+a QSpinBox.
+
+The `FloatSpinBoxDelegate` class is a custom item delegate that uses a
+QDoubleSpinBox to handle float values. This class can be used as a
+delegate for a QTableView or similar widget to allow for editing of
+float values within the view.
+
+Additional classes may be added to this module in the future to handle
+other data types.
+
+Example:
+    Here is an example of how to use `FloatSpinBoxDelegate`:
+
+    ```python
+    from PyQt6.QtWidgets import QTableView
+    from your_module import FloatSpinBoxDelegate
+
+    # Assuming you have a QAbstractTableModel `model`
+    view = QTableView()
+    view.setModel(model)
+
+    delegate = FloatSpinBoxDelegate(view)
+    view.setItemDelegate(delegate)
+    ```
+
+Note:
+    The range of the QDoubleSpinBox is currently set to
+    [-20.0000, 20.0000]. This can be adjusted as needed.
+"""
+
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import QDoubleSpinBox, QItemDelegate
+
+
+class FloatSpinBoxDelegate(QItemDelegate):
+    """Custom Item Delegate using a QSpinBox for float values."""
+
+    def __init__(self, decimals: int = 4, min: float = -20.0000,
+                 max: float = 20.000, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._decimals = decimals
+        self._min = min
+        self._max = max
+
+    def createEditor(self, parent, option, index):
+        """Create a QSpinBox as the editor for float values."""
+
+        editor = QDoubleSpinBox(parent)
+        editor.setFrame(False)
+        editor.setDecimals(self._decimals)
+        editor.setMinimum(self._min)
+        editor.setMaximum(self._max)
+        editor.setStepType(QDoubleSpinBox.StepType.AdaptiveDecimalStepType)
+        return editor
+
+    def setEditorData(self, spinBox, index) -> None:
+        """Set the data for the editor."""
+
+        value = index.model().data(index, Qt.ItemDataRole.EditRole)
+        spinBox.setValue(value)
+
+    def setModelData(self, spinBox, model, index) -> None:
+        """Set the model data."""
+
+        value = spinBox.value()
+        model.setData(index, value, Qt.ItemDataRole.EditRole)
--- server/ui/__init__.py
+++ server/ui/__init__.py
@@ -6,4 +6,5 @@ from .ProgramSettingsDialog import ProgramSettingsDialog
 from .EspSettingsDialog import EspSettingsDialog
 from .ContactGroupSettings import ContactGroupSettings
 
+from .Delegates import FloatSpinBoxDelegate
 from .uiHelpers import handleClosePrompt, handleDeletePrompt
