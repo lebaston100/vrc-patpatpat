08f5b8489b62ceecbe63e431c687e02923c7f4cd
lebaston100 <26111114+lebaston100@users.noreply.github.com>
2024-01-14 15:44:12 +0100

    refactor FileHelper to take str instead of Path

--- server/utils/FileHelper.py
+++ server/utils/FileHelper.py
@@ -1,23 +1,26 @@
 import json
-from utils.Logger import LoggerClass
 from pathlib import Path
 from typing import Any
 
+from utils.Logger import LoggerClass
+import logging
+
 logger = LoggerClass.getSubLogger(__name__)
+logger.setLevel(logging.INFO)
 
 
 class FileHelper:
     """A simple helper class to read and save json from files.
     """
 
-    def __init__(self, file: Path, *args, **kwargs) -> None:
+    def __init__(self, file: str, *args, **kwargs) -> None:
         """Initialize the file helper class.
 
         Args:
             file (Path): A pathlib.Path object to the config file.
         """
 
-        self._file = file
+        self._file = Path(file)
 
     def write(self, data: dict) -> bool:
         """Write all data into the configuration file.
@@ -32,7 +35,7 @@ class FileHelper:
             bool: True if the write was sucessful.
         """
 
-        # logger.debug("writing data to json file")
+        logger.debug("writing data to json file")
         try:
             with open(self._file, mode="w") as f:
                 json.dump(data, f, indent=4)
@@ -58,6 +61,19 @@ class FileHelper:
         except Exception as E:
             logger.exception(E)
             raise E
+
+    def hasData(self) -> bool:
+        """Check if the file exists and can be read.
+
+        Returns:
+            bool: True if the file exists and can be read,
+                False otherwise.
+        """
+
+        return self._file.is_file() and bool(self.read())
+
+    def initializeConfig(self):
+        pass
 
 
 if __name__ == "__main__":
