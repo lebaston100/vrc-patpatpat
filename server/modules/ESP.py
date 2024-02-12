61f3815cd5a60cee20af03dad163791834d27261
lebaston100 <26111114+lebaston100@users.noreply.github.com>
2024-02-12 22:32:00 +0100

    update ESP class

--- server/modules/ESP.py
+++ server/modules/ESP.py
@@ -1,14 +1,22 @@
 from PyQt6.QtCore import QObject
+from modules.GlobalConfig import GlobalConfigSingleton
 
 
 class ESP(QObject):
     """Represents a physical ESP"""
 
-    def __init__(self, key: str, settings: dict, parent: QObject | None = None) -> None:
-        super().__init__(parent)
+    def __init__(self, key: str) -> None:
+        super().__init__()
         self._key = key
-        self._id: int = settings["id"]
-        self._name: str = settings["name"]
-        self._lastIp: str = settings["lastIp"]
-        self._wifiMac: str = settings["wifiMac"]
-        self._numMotors: int = settings["numMotors"]
+        self._configKey = f"esps.{self._key}"
+        self._isConnected = False
+
+    def loadSettings(self, config: GlobalConfigSingleton):
+        self._id: int = config.get(f"{self._configKey}.id")
+        self._name: str = config.get(f"{self._configKey}.name", "")
+        self._lastIp: str = config.get(f"{self._configKey}.lastIp")
+        self._wifiMac: str = config.get(f"{self._configKey}.wifiMac")
+        self._numMotors: int = config.get(f"{self._configKey}.numMotors", 0)
+
+    # def writeSettings(self, config: GlobalConfigSingleton):
+        # config.set(f"{self._configKey}.lastIp", self._lastIp, True)
