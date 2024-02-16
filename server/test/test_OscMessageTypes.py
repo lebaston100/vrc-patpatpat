f80d9a8cbf6fe6b2d952a1dc85e2357021c0e418
lebaston100 <26111114+lebaston100@users.noreply.github.com>
2024-02-16 21:23:20 +0100

    add source field to osc message dataclasses

--- server/test/test_OscMessageTypes.py
+++ server/test/test_OscMessageTypes.py
@@ -18,26 +18,31 @@ class TestOscMessageTypes:
 
     @pytest.fixture()
     def HeartbeatMessageData(self):
-        return ("/patpatpat/heartbeat", ["AA:AA:AA:AA:AA:AA", 1, 2, 3])
+        return ("/patpatpat/heartbeat",
+                ["AA:AA:AA:AA:AA:AA", 1, 2, 3],
+                ["10.10.10.10", 2])
 
     @pytest.fixture()
     def validHeartbeatMessage(self, HeartbeatMessageData):
         """Yield a class-persistant HeartbeatMessage to test on"""
         from modules import HeartbeatMessage
-        yield HeartbeatMessage(*HeartbeatMessageData[1])
+        yield HeartbeatMessage(*HeartbeatMessageData[1],
+                               source=HeartbeatMessageData[2])
 
-    def test_HeartbeatMessage(self, validHeartbeatMessage, HeartbeatMessageData):
+    def test_HeartbeatMessage(self, validHeartbeatMessage,
+                              HeartbeatMessageData):
         from dataclasses import FrozenInstanceError
         from modules import HeartbeatMessage
         from datetime import datetime
 
         """Test if it can check it's own message"""
-        assert HeartbeatMessage.isType(*HeartbeatMessageData)
+        assert HeartbeatMessage.isType(*HeartbeatMessageData[:2])
 
         """Test if data is as expected"""
         m = validHeartbeatMessage
         assert m.mac == "AA:AA:AA:AA:AA:AA" \
             and m.uptime == 1 and m.vccBat == 2 and m.rssi == 3 \
+            and m.source == ["10.10.10.10", 2] \
             and isinstance(m.ts, datetime)
 
         """Test if object is mutable"""
@@ -46,24 +51,31 @@ class TestOscMessageTypes:
 
     @pytest.fixture()
     def DiscoveryResponseMessageData(self):
-        return ("/patpatpat/noticeme/senpai", ["AA:AA:AA:AA:AA:AA", 1])
+        return ("/patpatpat/noticeme/senpai",
+                ["AA:AA:AA:AA:AA:AA", 1],
+                ["10.10.10.10", 2])
 
     @pytest.fixture()
     def validDiscoveryResponseMessage(self, DiscoveryResponseMessageData):
         """Yield a class-persistant DiscoveryResponseMessage to test on"""
         from modules import DiscoveryResponseMessage
-        yield DiscoveryResponseMessage(*DiscoveryResponseMessageData[1])
+        yield DiscoveryResponseMessage(*DiscoveryResponseMessageData[1],
+                                       source=DiscoveryResponseMessageData[2])
 
-    def test_DiscoveryResponseMessage(self, validDiscoveryResponseMessage, DiscoveryResponseMessageData):
+    def test_DiscoveryResponseMessage(self, validDiscoveryResponseMessage,
+                                      DiscoveryResponseMessageData):
         from dataclasses import FrozenInstanceError
         from modules import DiscoveryResponseMessage
 
         """Test if it can check it's own message"""
-        assert DiscoveryResponseMessage.isType(*DiscoveryResponseMessageData)
+        assert DiscoveryResponseMessage.isType(
+            *DiscoveryResponseMessageData[:2])
 
         """Test if data is as expected"""
         m = validDiscoveryResponseMessage
-        assert m.mac == "AA:AA:AA:AA:AA:AA" and m.numMotors == 1
+        assert m.mac == "AA:AA:AA:AA:AA:AA" \
+            and m.numMotors == 1 \
+            and m.source == ["10.10.10.10", 2]
 
         """Test if object is mutable"""
         with pytest.raises(FrozenInstanceError):
