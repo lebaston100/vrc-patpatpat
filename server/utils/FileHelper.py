d6cdae627dea44734e1e3b64ef63c325440696ed
lebaston100 <26111114+lebaston100@users.noreply.github.com>
2024-03-01 10:12:03 +0100

    fix config getting corrupted by unserializable dict content

--- server/utils/FileHelper.py
+++ server/utils/FileHelper.py
@@ -1,9 +1,9 @@
 import json
+import logging
 from pathlib import Path
 from typing import Any
 
 from utils.Logger import LoggerClass
-import logging
 
 logger = LoggerClass.getSubLogger(__name__)
 logger.setLevel(logger.INFO)  # type: ignore
@@ -19,27 +19,30 @@ class FileHelper:
             file (Path): A pathlib.Path object to the config file.
         """
         self._file = Path(file)
+        self._tempfile = self._file.with_suffix(self._file.suffix + ".tmp")
 
     def write(self, data: dict) -> bool:
         """Write all data into the configuration file.
+        Avoids config corruption by writing into a temp file first.
 
         Args:
             data (dict): The data to write to the file.
-
-        Raises:
-            E (Exception): If there was an error writing the file.
 
         Returns:
             bool: True if the write was sucessful.
         """
         # logger.debug("writing data to json file")
         try:
-            with open(self._file, mode="w") as f:
+            with open(self._tempfile, mode="w") as f:
                 json.dump(data, f, indent=4)
-            return True
         except Exception as E:
             logger.exception(E)
-            raise E
+            if self._tempfile.exists():
+                self._tempfile.unlink()
+            return False
+        else:
+            self._tempfile.replace(self._file)
+            return True
 
     def read(self) -> dict[str, Any]:
         """Read all configration options from file.
